e41c656d40ba0d7bffd3c1041d7177ab0f6cc086
dwj <dwi@163.com>
2020-02-26 18:14:49 +0800

    fix

--- javascript/src/game.ts
+++ javascript/src/game.ts
@@ -36,14 +36,25 @@ export class Game {
 
   currentCategory() {
     const playerPlaces = this.getCurrentPlayerPlaces();
-
-    if (playerPlaces == 0 || playerPlaces == 4 || playerPlaces == 8)
-      return "Pop";
-    if (playerPlaces == 1 || playerPlaces == 5 || playerPlaces == 9)
-      return "Science";
-    if (playerPlaces == 2 || playerPlaces == 6 || playerPlaces == 10)
-      return "Sports";
-    return "Rock";
+    switch (playerPlaces) {
+      case 0:
+      case 4:
+      case 8: {
+        return "Pop";
+      }
+      case 1:
+      case 5:
+      case 9: {
+        return "Science";
+      }
+      case 2:
+      case 6:
+      case 10: {
+        return "Sports";
+      }
+      default:
+        return "Rock";
+    }
   }
 
   askQuestion() {
